7c6c9a69e6914d5efc47ed91dea1bd268099bfa8
MiltMont <miltgonmont@gmail.com>
2025-08-19 19:59:49 -0600

    Fix vec4 type definition

--- include/trx.h
+++ include/trx.h
@@ -5,44 +5,37 @@
 
 #ifndef __TRX_H__
 #define __TRX_H__
+
 #include <libosw/osw.h>
 
-#define CLAMP(x,a,b) x < a ? a : (x > b ? b : x)
+#define CLAMP(x, a, b) x < a ? a : (x > b ? b : x)
 
 /// Types
 typedef float vec3[3];
-
-typedef struct Vec4 {
-    float x; 
-    float y; 
-    float z; 
-    float w; 
-} vec4; 
-
+typedef float vec4[4];
 typedef float mat3[9];
-
 typedef float mat4[16];
 
 /* Vector related functions */
-f32* vec3_add(vec3 dest, const vec3 v, const vec3 u);
-f32* vec3_sub(vec3 dest, const vec3 v, const vec3 u);
-f32* vec3_mul(vec3 dest, const vec3 v, const vec3 u);
-f32* vec3_smul(vec3 dest, float k, const vec3 v);
-int   vec3_eq(const vec3 u, const vec3 v);
-f32* vec3_clamp(vec3 v, float a, float b);
-f32* vec3_lerp(vec3 dest, const vec3 v, const vec3 u, f32 a);
+f32 *vec3_add(vec3 dest, const vec3 v, const vec3 u);
+f32 *vec3_sub(vec3 dest, const vec3 v, const vec3 u);
+f32 *vec3_mul(vec3 dest, const vec3 v, const vec3 u);
+f32 *vec3_smul(vec3 dest, float k, const vec3 v);
+int vec3_eq(const vec3 u, const vec3 v);
+f32 *vec3_clamp(vec3 v, float a, float b);
+f32 *vec3_lerp(vec3 dest, const vec3 v, const vec3 u, f32 a);
 float vec3_dot(const vec3 v, const vec3 u);
-f32* vec3_cross(vec3 dest, const vec3 v, const vec3 u);
-f32* vec3_reflect(vec3 dest, const vec3 m, const vec3 p);
-void  vec3_normalize(vec3 v);
-f32* vec3_matMul(vec3 dest, const mat3 m, vec3 p);
-f32* vec3_mat3Mul(vec3 dest, const mat4 m, vec3 p);
+f32 *vec3_cross(vec3 dest, const vec3 v, const vec3 u);
+f32 *vec3_reflect(vec3 dest, const vec3 m, const vec3 p);
+void vec3_normalize(vec3 v);
+f32 *vec3_matMul(vec3 dest, const mat3 m, vec3 p);
+f32 *vec3_mat3Mul(vec3 dest, const mat4 m, vec3 p);
 float vec3_mat4Mul(vec3 dest, const mat4 m, vec3 p);
-f32* vec3_homogenize(vec3 v, float w);
+f32 *vec3_homogenize(vec3 v, float w);
 
-vec4  vec4_normalize(vec4 v);
-vec4  vec4_mat4Mul(vec4 dest, const mat4 m, vec4 p);
-vec4  vec4_homogenize(vec4 v);
+f32 vec4_normalize(vec4 v);
+f32 vec4_mat4Mul(vec4 dest, const mat4 m, vec4 p);
+f32 vec4_homogenize(vec4 v);
 
 /* Matrix related functions */
 void mat4_identity(mat4 m);
@@ -55,6 +48,5 @@ void mat4_scale(mat4 m, vec3 v);
 
 void mat4_ortho(mat4 m, float left, float right, float bottom, float top, float near, float far);
 void mat4_perspective(mat4 m, float fovy, float aspect, float znear, float zfar);
-
 
 #endif /*__TRX_H__*/
