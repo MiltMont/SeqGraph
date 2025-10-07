160f55c5d6dc2f8f8dc79945eea9d2d1d4e96b20
MiltMont <miltgonmont@gmail.com>
2025-10-07 23:12:18 +0000

    Simplicial complex rendering

--- src/main.c
+++ src/main.c
@@ -10,26 +10,32 @@ int main(int argc, char *argv[]) {
     return err;
   }
 
-  Color background = 0xfffffffff;
+  Color background = 0x00;
 
   sgSetClearColor(background);
   sgViewport(0, 0, W, H);
 
   f32 timer = 0;
 
-  Vertex a = {.position = {0.0, 0.0, 2.0}, .color = {1.0, 0.0, 0.0}};
-  Vertex b = {.position = {1.0, 0.0, 2.0}, .color = {1.0, 0.0, 0.0}};
-  Vertex c = {.position = {0.0, 1.0, 2.0}, .color = {1.0, 0.0, 0.0}};
-  Vertex d = {.position = {1.0, 1.0, 2.0}, .color = {1.0, 0.0, 0.0}};
+  Vertex zero = {.position = {0.0, 0.0, 2.0}, .color = {1.0, 0.0, 0.0}};
+  Vertex e_x = {.position = {1.0, 0.0, 2.0}, .color = {0.0, 1.0, 0.0}};
+  Vertex e_y = {.position = {0.0, 1.0, 2.0}, .color = {0.0, 0.0, 1.0}};
+  Vertex e_z = {.position = {0.0, 0.0, 4.0}, .color = {1.0, 0.0, 0.0}};
 
-  Vertex points[] = {a, b, c, b, c, d};
+  Vertex points[] = {
+    e_x, e_y, e_z, // f1 
+    e_x, zero, e_y, // f2
+    e_x, zero, e_z,  // f3
+    zero, e_x, e_z
+  };
+
 
   while (1) {
     timer += 0.01;
 
     sgClearColor();
     OSW_Poll();
-    sgDrawVertex(sgTriangle, points, 6);
+    sgDrawVertex(sgTriangle, points, 12);
     sgDrawBuffer();
     OSW_VideoSwapBuffers();
   }
